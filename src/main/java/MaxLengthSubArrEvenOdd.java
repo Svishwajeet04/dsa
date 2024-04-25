b9d9b5c769feec2ef0bc79ede8409b7f408d5a17
Vishwejeet Singh <vishwejeet.singh@iot83.com>
2024-04-25 09:44:11 +0530

    25-04-2024

---
+++ src/main/java/MaxLengthSubArrEvenOdd.java
@@ -0,0 +1,35 @@
+public class MaxLengthSubArrEvenOdd {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{34, 45, 67, 22, 44, 66, 88, 23, 56, 34, 79};
+        System.out.println(solve(arr));
+    }
+
+    private static int solve(int[] arr) {
+        if(arr == null){
+            throw new RuntimeException("arr is null");
+        }else{
+            int maxLen = 0;
+            int currLen = 1;
+            for (int i = 1; i < arr.length; i++) {
+                if(arr[i] % 2 == 0){
+                    if(arr[i-1] % 2 != 0){
+                        currLen++;
+                    }else{
+                        currLen = 1;
+                    }
+                }else{
+                    if(arr[i-1] % 2 == 0){
+                        currLen++;
+                    }else{
+                        currLen = 1;
+                    }
+                }
+                if(currLen > maxLen){
+                    maxLen = currLen;
+                }
+            }
+            return maxLen;
+        }
+    }
+}
