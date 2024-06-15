f2ac359d2598b76ead6b6c08988f61082eb57bf3
Vishwejeet Singh <vishwejeet.singh@iot83.com>
2024-06-15 08:17:59 +0530

    15-06-2024

---
+++ src/main/java/sorting/QsPartition.java
@@ -0,0 +1,27 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class QsPartition {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 6, 12, 10, 7, 7};
+        int index = 6;
+        System.out.println(Arrays.toString(partition(arr, index)));
+    }
+
+    private static int[] partition(int[] arr, int index) {
+        int pivot = arr[index];
+        for (int i = 0, j = arr.length - 1; i < j; ) {
+            if(arr[i] <= pivot){
+                i++;
+            }else{
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+}
