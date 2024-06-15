b9e919ab2d3d5ee61aa06550d2542273d6c6612d
Vishwejeet Singh <vishwejeet.singh@iot83.com>
2024-06-15 08:19:07 +0530

    15-06-2024

--- src/main/java/sorting/QsPartition.java
+++ src/main/java/sorting/QsPartition.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class QsPartition {
 
     public static void main(String[] args) {
-        int[] arr = {3, 8, 6, 12, 10, 7, 7};
+        int[] arr = {3, 8, 6, 12, 9 , 55, 10, 7, 7};
         int index = 6;
         System.out.println(Arrays.toString(partition(arr, index)));
     }
